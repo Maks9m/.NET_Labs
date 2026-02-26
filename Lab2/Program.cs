4c2ca8a22f70f544fd0828eaf9df555eb1f84516
Maks9m <maxim.kramarenko8a@gmail.com>
2026-02-26 15:56:33 +0200

    chore(lab2): Add example for lab2, init project

---
+++ Lab2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          //
+        }
+    }
+}
